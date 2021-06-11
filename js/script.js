54597d9e8d61750e7dc50c9f09534786d3140d22
Piero <piero.onorati@virgilio.it>
2021-06-11 17:39:29 +0200

    complete Algorithm

--- js/script.js
+++ js/script.js
@@ -5,52 +5,61 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto pr
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
 
 
-var pcNumbers = [];
+var pcNumbers = [], userNumber = [], found = [], result ;
 
+// Algorithm to generate pc numbers
 while (pcNumbers.length < 5) {
-
     var numRand = getRndInteger(1, 100);
     if (!pcNumbers.includes(numRand)) {
-        pcNumbers.push(numRand) 
+        pcNumbers.push(numRand);
     }
 }
 
 alert(pcNumbers);
 
-var userNumber = [];
+
+var timeleft = 10;
 
 
-
-
-var timeleft = 30;
 
 var countdown = setInterval(function(){
 
   if(timeleft == 0){
     clearInterval(countdown);
     document.getElementById("countdown").innerHTML = "Finished";
+
+
+    // Algorithm to generate user numbers
+    setTimeout (function(){
+        while (userNumber.length < 5) {
+
+            var number = parseInt(prompt('Inserisci un numero'));
+
+            // Check on Input numbers
+            if(isNaN(number) || number <= 0 || number > 100) {
+                alert('Puoi inserire solo i numeri da 1 a 100!');
+            }
     
-    while (userNumber.length < 5) {
+            if (!userNumber.includes(number)) {
+                userNumber.push(number);
+            } else{
+                alert('Hai già inserito questo numero');
+            }
 
-        var number = parseInt(prompt('Inserisci un numero'));
-
-        if(isNaN(number) || number <= 0 || number > 100) {
-            alert('Puoi inserire solo i numeri da 1 a 100!')
+            // final result: number found 
+            if (pcNumbers.includes(number)) {
+                found.push(number);
+            }
+            result = document.getElementById("result").innerHTML = 'hai indovitato ' + found.length + ' numeri:' + found;
         }
 
-        if (!userNumber.includes(number)) {
-            userNumber.push(number);
-        } else{
-            alert('Hai già inserito questo numero');
-        }
-    }
+    }, 1000);
 
   } else {
     document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
   }
 
   timeleft -= 1;
-
   
 }, 1000);
 
